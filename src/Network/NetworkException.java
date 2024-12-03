b19eb2869e50bb5acf4fc795324d8489e6160a54
Liam Tangney <ltags230@gmail.com>
2024-12-02 19:52:07 -0600

    added network exception

---
+++ src/Network/NetworkException.java
@@ -0,0 +1,11 @@
+/*
+ * Author: Liam D. Tangney
+ */
+
+package Network;
+
+public class NetworkException extends Exception {
+    public NetworkException(String message) {
+        super(message);
+    }
+}
